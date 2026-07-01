e4ae33a969f830e1f3dfcce9d08bf41aae8d13db
meowpow-png <marin.softic@proton.me>
2026-07-01 14:55:49 +0200

    Add core backend dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    id("org.springframework.boot") version "4.0.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "hr.ht.rnd"
@@ -14,4 +16,19 @@ java {
 
 repositories {
     mavenCentral()
+}
+
+val cxfCodegen = configurations.create("cxfCodegen")
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.cxf.spring.boot.starter.jaxws)
+    implementation(libs.springdoc.openapi.starter.webmvc.ui)
+
+    cxfCodegen(libs.cxf.tools.wsdlto.core)
+    cxfCodegen(libs.cxf.tools.wsdlto.frontend.jaxws)
+    cxfCodegen(libs.cxf.tools.wsdlto.databinding.jaxb)
+
+    testImplementation(libs.spring.boot.starter.test)
 }
