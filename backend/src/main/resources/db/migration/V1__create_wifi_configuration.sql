5db151ff86df372bb4333d03c011fa5ab12d984e
meowpow-png <marin.softic@proton.me>
2026-07-03 10:03:36 +0200

    Create wifi configuration migration

---
+++ backend/src/main/resources/db/migration/V1__create_wifi_configuration.sql
@@ -0,0 +1,11 @@
+-- ============================
+-- wifi_configuration table
+-- ============================
+
+CREATE TABLE wifi_configuration (
+    cpe_id VARCHAR PRIMARY KEY,
+    wifi_band VARCHAR NOT NULL,
+    ssid VARCHAR NOT NULL,
+    encryption_type VARCHAR NOT NULL,
+    password VARCHAR
+);
